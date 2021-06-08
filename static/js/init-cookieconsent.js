88264860d49bf459bd17805c01d339091ec9454e
Kreee Jiang <contact@ohmykreee.top>
2021-06-08 17:03:58 +0800

    Fix google analytics and add cookie disclaimer

---
+++ static/js/init-cookieconsent.js
@@ -0,0 +1,13 @@
+// https://cookieconsent.insites.com/download/#
+window.addEventListener('load', function () {
+  window.cookieconsent.initialise({
+    'palette': {
+      'popup': {
+        'background': '#1D1F38'
+      },
+      'button': {
+        'background': '#F1F1F1'
+      }
+    }
+  })
+})
