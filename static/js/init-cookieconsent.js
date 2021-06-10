672c64a2f1b8c4a62940c8fb33b3d7c375483823
Kreee Jiang <yangke.jiang@outlook.com>
2021-06-10 10:31:44 +0800

    Translate cookie disclaimer

--- static/js/init-cookieconsent.js
+++ static/js/init-cookieconsent.js
@@ -1,13 +1,17 @@
-// https://cookieconsent.insites.com/download/#
-window.addEventListener('load', function () {
-  window.cookieconsent.initialise({
-    'palette': {
-      'popup': {
-        'background': '#1D1F38'
-      },
-      'button': {
-        'background': '#F1F1F1'
-      }
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#2d3642"
+    },
+    "button": {
+      "background": "#444984"
     }
-  })
-})
+  },
+  "theme": "edgeless",
+  "position": "bottom-right",
+  "content": {
+    "message": "该网站储存诸如cookie之类的数据，以启用基本的网站功能，个性化和分析功能。",
+    "dismiss": "明白",
+    "link": "了解更多"
+  }
+});
