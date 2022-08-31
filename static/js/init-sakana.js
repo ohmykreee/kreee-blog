ed5140ec4dcc722368fbc869b3a4833e71029e23
Kreee <contact@ohmykreee.top>
2022-08-31 20:07:51 +0800

    Fix: fix more typo of sakana :(

--- static/js/init-sakana.js
+++ static/js/init-sakana.js
@@ -16,12 +16,12 @@ var loadSakanaWidget = () => {
 if (window.matchMedia("(min-width: 800px)").matches) {
   loadSakanaWidget()
 } else {
-  if (document.getElementById('ifLoadSakna')) {
-    const loaderDiv = document.getElementById('ifLoadSakna')
+  if (document.getElementById('ifLoadSakana')) {
+    const loaderDiv = document.getElementById('ifLoadSakana')
     const loaderA = document.createElement('a')
     loaderA.innerHTML = '点此加载左下角的小组件...'
     loaderA.setAttribute('href','#')
-    loaderA.setAttribute('onclick', `loadSakanaWidget();document.getElementById('ifLoadSakna').remove();return false;`)
+    loaderA.setAttribute('onclick', `loadSakanaWidget();document.getElementById('ifLoadSakana').remove();return false;`)
     loaderDiv.appendChild(loaderA)
   }
 }
